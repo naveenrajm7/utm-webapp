63de0f010fe8750ed9863cfaa93b8b1e6379b5c3
Naveenraj M <22456988+naveenrajm7@users.noreply.github.com>
2024-12-21 23:04:47 -0700

    UTM UI * Use bootstrap for css * Use Tailwind with prefix to avoid conflict

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,3 +1,8 @@
+import "./globals.css";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +10,31 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div className="d-flex">
+                <nav className="sidebar col-3">
+                    <header>Virtual Machines</header>
+                    <a href="#">LinuxKernel</a>
+                    <a href="#">micro</a>
+                    <a href="#">RHEL</a>
+                    <a href="#">QemuSnap</a>
+                    <a href="#">macOS</a>
+                    <a href="#">Windows11</a>
+                    <a href="#">DebianCloud</a>
+                </nav>
+                <div className="main-content col-9">
+                    <header className="main-content-header d-flex align-items-center" style={{ backgroundColor: 'inherit', color: 'inherit', padding: '0.5rem 1rem' }}>
+                        <button className="btn btn-primary" style={{ backgroundColor: 'inherit', border: 'none', color: 'inherit', padding: 0 }}>
+                            <i className="bi bi-plus" style={{ fontSize: '1.5rem', color: '#ffffff' }}></i>
+                        </button>
+                        <span className="ml-3" style={{color: '#ffffff'}}>UTM</span>
+                    </header>
+                    <main>
+                        {children}
+                    </main>
+                </div>
+            </div>
+      </body>
     </html>
   );
 }
