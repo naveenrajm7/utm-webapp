fb8ec9ad7da7ed331a3ce038a26eefa3ff6fff98
Naveenraj M <22456988+naveenrajm7@users.noreply.github.com>
2024-12-23 23:16:43 -0700

    terminal in browser. * create terminal client app using xterm.js * backend(ws): add websocket server which connects to terminal client to exchange data using ws * backend(ws): for every client connection spawn a pseudo terminal process using node-pty * backend : both backend services, REST API and WebSocket run as a single express app (process) on a single port. https://stackoverflow.com/questions/13791050/is-it-possible-to-enable-tcp-http-and-websocket-all-using-the-same-port

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,51 @@
 import { createServer } from "./server";
 import { log } from "@repo/logger";
+import http from "http";
+import { WebSocketServer } from "ws";
+import { spawn } from "node-pty";
 
+// one port, one express app
 const port = process.env.PORT || 3001;
-const server = createServer();
+const app = createServer();
+
+// API server
+const server = http.createServer(app);
+// WebSocket server
+const wss = new WebSocketServer({ server });
 
 server.listen(port, () => {
   log(`api running on ${port}`);
+});
+
+// on every connection, spawn a new zsh process
+wss.on("connection", (ws) => {
+  const ptyProcess = spawn("zsh", [], {
+    name: "xterm-color",
+    env: process.env,
+  });
+
+  // on every message, send it to the bash process
+  ws.on("message", (message) => {
+    console.log(`received: ${message}`);
+    // websocket messages are strings, so we need to parse them
+    const data = JSON.parse(message.toString());
+
+    if (data.type === "command") {
+      ptyProcess.write(data.data);
+    }
+  });
+
+  ptyProcess.onData((data) => {
+    const message = JSON.stringify({
+      type: "data",
+      data,
+    });
+
+    ws.send(message);
+  });
+
+  ws.on("close", () => {
+    console.log("closed ws");
+  });
+
 });
